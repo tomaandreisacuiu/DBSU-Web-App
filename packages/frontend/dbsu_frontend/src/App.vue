95601c39925cc4f10c0c6cf572bcef8e2da0e63b
Toma Andrei Sacuiu <tomaandreisacuiu@gmail.com>
2024-03-10 18:44:59 +0100

    devices cards design done

--- packages/frontend/dbsu_frontend/src/App.vue
+++ packages/frontend/dbsu_frontend/src/App.vue
@@ -1,13 +1,41 @@
 <script setup>
+  import { ref, provide } from 'vue';
 import TopNav from './components/TopNav.vue';
+import DeviceCard from './components/DeviceCard.vue';
+
+const devices = ref([
+    {name: 'Device1', song: 'Song A'},
+    {name: 'Device2', song: 'Song B'},
+    {name: 'Device3', song: 'Song C'},
+])
+
+const currentIndex = ref(null);
+const playSong = (index) => {
+  currentIndex.value = index;
+};
+
+// Provide the playSong function and currentIndex to child components
+provide('playSong', playSong);
+provide('currentIndex', currentIndex);
+
 </script>
 
 <template>
-<div>
+<div id="app-body">
     <TopNav/>
+
+    <div class="cards">
+        <DeviceCard
+        v-for="(device, index) in devices"
+        :key="index"
+        :deviceName="device.name"
+        :songName="device.song"
+        :isPlaying="currentIndex === index"
+        @play="playSong(index)"
+        />
+    </div>
 </div>
 </template>
 
 <style scoped>
-
 </style>
---
+++ packages/frontend/dbsu_frontend/src/components/DeviceCard.vue
@@ -0,0 +1,51 @@
+<script setup>
+    const props = defineProps({
+        deviceName: String,
+        songName: String,
+        isPlaying: Boolean
+    })
+
+    const playPauseSong = () => {
+        if (!props.isPlaying) {
+            console.log("play song");
+        } else {
+            console.log("stop current song, start the new one")
+        }
+    }
+</script>
+
+<template>
+    <div class = "device-card">
+
+        <div class = "song-information">
+            <h3>{{ deviceName }}</h3>
+            <h5>{{ songName }}</h5>
+        </div>
+
+        <button id="button" @click="playPauseSong">{{ isSongPlaying ? 'Pause' : 'Play' }}</button>
+
+    </div>
+</template>
+
+<style scoped>
+.device-card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  border: 0px solid #ccc;
+  margin-bottom: 10px;
+  background-color: #D38F06;
+  width: 50vh;
+}
+
+.song-information h3,
+.song-information h5 {
+  margin: 0;
+  color: #3A3933;
+}
+
+#button {
+    background-color: #BD2218;
+}
+</style>
